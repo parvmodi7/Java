376108586e952d3f08b7dc25a172fbcb08f2832c
Garvis123 <parvmodi11@gmail.com>
2026-04-10 22:59:43 +0530

    Ad

--- Javabasic.java
+++ Javabasic.java
@@ -2,8 +2,24 @@ import java.util.Arrays;
 
 public class Javabasic {
     public static void main(String[] args) {
-   String str="abcd";
-   
+String s="abcdefgh";
+int vowels = 0;
+int con=0;
+for(int i=0;i<s.length();i++)
+{
+    if(s.charAt(i)=='a'||s.charAt(i)=='i'|| s.charAt(i)=='o'|| s.charAt(i)=='u'|| s.charAt(i)=='e')
+    {
+           vowels++;
+    }
+    else
+    {
+     con++;
+    }
+}
+System.out.print(vowels);
+System.out.print(con);
+
+
 
 
    
