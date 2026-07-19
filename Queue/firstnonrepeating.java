17b92d7ef05e358d1ba7e53281f3194898cfaa2f
Garvis123 <parvmodi11@gmail.com>
2026-07-20 01:17:55 +0530

    ok

---
+++ Queue/firstnonrepeating.java
@@ -0,0 +1,29 @@
+package Queue;
+import java.util.*;
+public class firstnonrepeating {
+    public static void main(String args[])
+    {
+        String s ="aabccxb";
+        int freq[]=new int[26];
+        Queue <Character> q = new LinkedList<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']>1)
+            {
+                q.remove();
+            }
+            if(q.isEmpty())
+            {
+                System.out.print(-1);
+            }
+            else
+            {
+                System.out.print(q.peek() + " ");
+            }
+        }
+
+    }
+}
